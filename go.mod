30b29e7c026c75b5f425608448a0f54e2ef8d03d
Jimmy Huang <jimmy@netivism.com.tw>
2023-07-23 17:24:30 +0800

    Formalize response struct.

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/netivism/goshort
+
+go 1.20
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/pkg/errors v0.9.1
+	github.com/speps/go-hashids/v2 v2.0.1
+	go.etcd.io/bbolt v1.3.7
+)
+
+require golang.org/x/sys v0.4.0 // indirect
--- main.go
+++ main.go
@@ -6,15 +6,18 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
-	"time"
 	"os"
+	"regexp"
+	"strconv"
+	"time"
+
+	"crypto/sha256"
+	"crypto/subtle"
 
 	"github.com/gorilla/mux"
 	"github.com/pkg/errors"
-	"github.com/speps/go-hashids"
+	"github.com/speps/go-hashids/v2"
 	bolt "go.etcd.io/bbolt"
-	"crypto/sha256"
-  "crypto/subtle"
 )
 
 var (
@@ -29,6 +32,12 @@ type GoShort struct {
 	Count    int    `json:"count"`
 }
 
+type GoShortResp struct {
+	Success int       `json:"success"`
+	Message string    `json:"message"`
+	Result  []GoShort `json:"result,omitempty"`
+}
+
 type Authenticate struct {
 	username string
 	password string
@@ -39,9 +48,19 @@ func BasicAuth(next http.HandlerFunc) http.HandlerFunc {
 	auth.username = os.Getenv("AUTH_USERNAME")
 	auth.password = os.Getenv("AUTH_PASSWORD")
 
+	if auth.username == "" {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			HandlerError(w, "Page not available.", http.StatusForbidden)
+		})
+	}
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		username, password, ok := r.BasicAuth()
 		if ok {
+			if username == "" {
+				HandlerError(w, "No user name provided", http.StatusUnauthorized)
+				return
+			}
 			usernameHash := sha256.Sum256([]byte(username))
 			passwordHash := sha256.Sum256([]byte(password))
 			expectedUsernameHash := sha256.Sum256([]byte(auth.username))
@@ -62,113 +81,166 @@ func BasicAuth(next http.HandlerFunc) http.HandlerFunc {
 }
 
 func ListEntries(w http.ResponseWriter, req *http.Request) {
-	w.WriteHeader(200)
-	fmt.Fprint(w, "Successful logged\n")
+	queryParams := req.URL.Query()
+	offsetStr := queryParams.Get("offset")
+	offset, err := strconv.Atoi(offsetStr)
+	if err != nil {
+		offset = 0
+	}
+	limit := 100
+
+	result := make([]GoShort, 0)
 
 	db.View(func(tx *bolt.Tx) error {
 		bucket := tx.Bucket(goshortBucket)
 		c := bucket.Cursor()
 
-		count := 0
+		count := -1
 		for k, v := c.First(); k != nil; k, v = c.Next() {
-			fmt.Fprintf(w, "key=%s, value=%s\n", k, v)
 			count++
+			if count < offset {
+				continue
+			}
+			var val GoShort
+			err := json.Unmarshal(v, &val)
+
+			if err == nil {
+				result = append(result, val)
+				if len(result) > limit {
+					break
+				}
+			}
 		}
 
-	  fmt.Fprintf(w, "Total num rows: %d rows\n", count)
 		return nil
 	})
+	if len(result) > 0 {
+		HandlerSuccess(w, "Entries loaded successfully.", result, http.StatusOK)
+	} else {
+		HandlerSuccess(w, "No entry found.", result, http.StatusOK)
+	}
 }
 
 // CreateEntry route to create shorten entry
 func CreateEntry(w http.ResponseWriter, req *http.Request) {
+	defer req.Body.Close()
+
 	var goshort GoShort
 	err := json.NewDecoder(req.Body).Decode(&goshort)
 	if err != nil {
-		NotFound(w)
-		err = errors.New("Err: could not decode entry")
-		log.Printf("%s\n", err)
+		HandlerError(w, "Could not decode entry", http.StatusBadRequest)
 		return
 	}
-	if goshort.Redirect != "" {
-		hd := hashids.NewData()
-		limit := 0
-		now := time.Now().UnixNano()
-		uhash := ""
-		for limit < 5 {
-			hd.Salt = fmt.Sprintf("%d", rand.Intn(10000000))
-			h, _ := hashids.NewWithData(hd)
-			uhash, _ = h.Encode([]int{int(now)})
-			log.Printf("%s hashed generated.\n", uhash);
-			err := db.View(func(tx *bolt.Tx) error {
-				bucket := tx.Bucket(goshortBucket)
-				raw := bucket.Get([]byte(uhash))
-				if raw == nil {
-					return nil
-				}
-				time.Sleep(1 * time.Second)
-				err := errors.New("Err: duplicate entry or " + uhash + ", continue")
-				return err
-			})
-			if err == nil {
-				break
-			} else {
-				log.Printf("Continue next loop because %s\n", err)
-			}
-			limit++
-		}
 
-		err = db.Update(func(tx *bolt.Tx) error {
-			if len(uhash) <= 0 {
-				return errors.Wrap(err, "No short string")
-			} else {
-				goshort.Short = uhash
-			}
-			bucket := tx.Bucket(goshortBucket)
-			goshort.Count = 1
-			jurl, err := json.Marshal(goshort)
-			if err != nil {
-				return errors.Wrap(err, "Err: could not marshal entry")
-			}
-			err = bucket.Put([]byte(uhash), jurl)
-			log.Println("SUCCESS: New entry created - " + string(jurl))
-			if err != nil {
-				return errors.Wrap(err, "Err: could not put data into bucket")
-			}
-			return nil
-		})
-		if err != nil {
-			log.Printf("%s\n", err)
-			NotFound(w)
-		} else {
-			json.NewEncoder(w).Encode(goshort)
-		}
-	} else {
-		NotFound(w)
-		err = errors.New("Err: entry missing redirect address")
-		log.Printf("%s\n", err)
+	if goshort.Redirect == "" {
+		HandlerError(w, "Entry missing redirect address", http.StatusBadRequest)
+		return
 	}
-}
 
-// Root route handler. will get all path by id
-func Root(w http.ResponseWriter, req *http.Request) {
-	params := mux.Vars(req)
-	var goshort GoShort
-	err := db.View(func(tx *bolt.Tx) error {
+	uhash, err := GenerateUniqueHash()
+	if err != nil {
+		HandlerError(w, fmt.Sprintf("Hash generation error. %v", err), http.StatusBadRequest)
+		return
+	}
+
+	err = db.Update(func(tx *bolt.Tx) error {
 		bucket := tx.Bucket(goshortBucket)
-		raw := bucket.Get([]byte(params["id"]))
-		if raw == nil {
-			return errors.New("No entry found")
-		}
-		log.Printf("SUCCESS: visit %s\n", raw)
-		err := json.Unmarshal(raw, &goshort)
+		goshort.Short = uhash
+		goshort.Count = 1
+		jurl, err := json.Marshal(goshort)
 		if err != nil {
-			return errors.Wrap(err, "could not unmarshal entry")
+			return errors.Wrap(err, "Err: could not marshal entry")
+		}
+		err = bucket.Put([]byte(uhash), jurl)
+		log.Println("SUCCESS: New entry created - " + string(jurl))
+		if err != nil {
+			return errors.Wrap(err, "Err: could not put data into bucket")
 		}
 		return nil
 	})
 
-	if err == nil {
+	if err != nil {
+		HandlerError(w, fmt.Sprintf("Error when saving record. %v", err), http.StatusInternalServerError)
+	} else {
+		result := []GoShort{goshort}
+		HandlerSuccess(w, "URL shorten successfully.", result, http.StatusCreated)
+	}
+}
+
+// GenerateUniqueHash generates a unique hash using hashids library.
+// It returns the generated hash as a string and any error encountered during the process.
+func GenerateUniqueHash() (string, error) {
+	tries := 0
+	now := time.Now().Unix()
+
+	for tries < 5 {
+		hd := hashids.NewData()
+		hd.MinLength = 5
+		hd.Salt = fmt.Sprintf("%d", rand.Intn(10000000))
+		h, _ := hashids.NewWithData(hd)
+		uhash, _ := h.Encode([]int{int(now)})
+
+		err := db.View(func(tx *bolt.Tx) error {
+			bucket := tx.Bucket(goshortBucket)
+			raw := bucket.Get([]byte(uhash))
+			if raw == nil {
+				return nil
+			}
+			time.Sleep(1 * time.Second)
+			err := errors.New("Duplicate entry of " + uhash + ", continue")
+			return err
+		})
+
+		if err == nil {
+			return uhash, nil
+		} else {
+			log.Printf("Continue next loop because %s\n", err)
+		}
+
+		tries++
+	}
+
+	return "", errors.New("Unable to generate unique hash")
+}
+
+// Root route handler. will get all path by id
+func Redirect(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	shortenId := params["id"]
+	if shortenId == "" {
+		HandlerError(w, "Invalid ID", http.StatusNotFound)
+		return
+	}
+
+	if len(shortenId) > 20 {
+		HandlerError(w, "Invalid ID", http.StatusNotFound)
+		return
+	}
+
+	regex := regexp.MustCompile("^[a-zA-Z0-9]+$")
+	match := regex.MatchString(shortenId)
+	if !match {
+		HandlerError(w, "Invalid ID", http.StatusNotFound)
+		return
+	}
+
+	var goshort GoShort
+	err := db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(goshortBucket)
+		raw := bucket.Get([]byte(shortenId))
+		if raw == nil {
+			return errors.New("No entry of " + shortenId)
+		}
+		err := json.Unmarshal(raw, &goshort)
+		if err != nil {
+			return errors.Wrap(err, "Entry format error.")
+		}
+		return nil
+	})
+
+	if err != nil {
+		HandlerError(w, fmt.Sprintf("URL not found. %s", err), http.StatusNotFound)
+	} else {
 		err = db.Update(func(tx *bolt.Tx) error {
 			bucket := tx.Bucket(goshortBucket)
 			goshort.Count++
@@ -185,17 +257,35 @@ func Root(w http.ResponseWriter, req *http.Request) {
 		if err != nil {
 			log.Printf("%s\n", err)
 		}
-		http.Redirect(w, req, goshort.Redirect, 301)
-	} else {
-		NotFound(w)
-		log.Printf("%s\n", err)
+		http.Redirect(w, req, goshort.Redirect, http.StatusMovedPermanently)
 	}
 }
 
-// NotFound is fallback function when entry not found
-func NotFound(w http.ResponseWriter) {
-	w.WriteHeader(404)
-	fmt.Fprint(w, "404 not found")
+// HandlerError is fallback function when entry not found
+func HandlerError(w http.ResponseWriter, message string, status int) {
+	err := errors.New("Err: " + message)
+	log.Printf("%s\n", err)
+	result := make([]GoShort, 0)
+
+	goShortResp := GoShortResp{
+		Success: 0,
+		Message: message,
+		Result:  result,
+	}
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(goShortResp)
+}
+
+func HandlerSuccess(w http.ResponseWriter, message string, result []GoShort, status int) {
+	goShortResp := GoShortResp{
+		Success: 1,
+		Message: message,
+		Result:  result,
+	}
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(goShortResp)
 }
 
 func main() {
@@ -209,11 +299,13 @@ func main() {
 	})
 
 	router := mux.NewRouter()
-
 	router.HandleFunc("/handle/list-entries", BasicAuth(ListEntries)).Methods("GET")
-	router.HandleFunc("/handle/create-entry", CreateEntry).Methods("PUT")
-	router.HandleFunc("/{id}", Root).Methods("GET")
+	// router.HandleFunc("/handle/list-entry/{id}", BasicAuth(ListEntries)).Methods("GET")
+	// router.HandleFunc("/handle/update-entry/{id}", BasicAuth(ListEntries)).Methods("PUT")
+	router.HandleFunc("/handle/create-entry", CreateEntry).Methods("POST")
+	router.HandleFunc("/{id}", Redirect).Methods("GET")
 	log.Fatal(http.ListenAndServe(":33512", router))
+	log.Println("Server started at port 33512")
 
 	defer db.Close()
 }
