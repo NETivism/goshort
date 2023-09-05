509ea4bd53ddcc908128eff0286cbf4625a7cbcc
Jimmy Huang <jimmy@netivism.com.tw>
2023-09-05 19:48:33 +0800

    Add boltdb migration feature

--- backend/main.go
+++ backend/main.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"net/http"
 
-	// blt "github.com/netivism/goshort/backend/pkg/bolt"
 	"github.com/netivism/goshort/backend/pkg/db"
 	"github.com/netivism/goshort/backend/pkg/env"
 	"github.com/netivism/goshort/backend/pkg/restapi"
@@ -18,11 +17,14 @@ func main() {
 	}
 
 	db.Connect()
-	// blt.New()
-	router := restapi.New()
-	port := ":" + env.Get(env.ListenPort)
-	fmt.Printf("HTTP server startup and listening on port%s\n", port)
-	log.Fatal(http.ListenAndServe(port, router))
 
-	// blt.Close()
+	// doing migration from bolddb to sqlite
+	if env.Get(env.DatabaseMigrate) != "" {
+		db.Migrate()
+	} else {
+		router := restapi.New()
+		port := ":" + env.Get(env.ListenPort)
+		fmt.Printf("HTTP server startup and listening on port%s\n", port)
+		log.Fatal(http.ListenAndServe(port, router))
+	}
 }
--- backend/pkg/db/main.go
+++ backend/pkg/db/main.go
@@ -1,7 +1,10 @@
 package db
 
 import (
+	"encoding/json"
 	"log"
+	"net/url"
+	"os"
 	"sync"
 
 	//	"gorm.io/driver/mysql"
@@ -9,8 +12,11 @@ import (
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 
+	blt "github.com/netivism/goshort/backend/pkg/bolt"
 	"github.com/netivism/goshort/backend/pkg/env"
+	"github.com/netivism/goshort/backend/pkg/goshort"
 	"github.com/netivism/goshort/backend/pkg/model"
+	bolt "go.etcd.io/bbolt"
 )
 
 var (
@@ -47,4 +53,40 @@ func Connect() (*gorm.DB, error) {
 	})
 
 	return instance, err
+}
+
+func Migrate() {
+	if _, err := os.Stat("goshort.db"); err == nil {
+		blt.New()
+		dbi, err := Connect()
+		if err != nil {
+			log.Fatal("Migration stopped because destination database connection failed.")
+		}
+		log.Printf("Start database migration")
+
+		blt.DB.View(func(tx *bolt.Tx) error {
+			bucket := tx.Bucket(blt.GoshortBucket)
+			c := bucket.Cursor()
+			for k, v := c.First(); k != nil; k, v = c.Next() {
+				var val goshort.GoShort
+				err := json.Unmarshal(v, &val)
+				if err == nil {
+					r, _ := url.Parse(val.Redirect)
+					record := model.Redirect{
+						Id:       val.Short,
+						Redirect: val.Redirect,
+						Domain:   r.Host,
+						Path:     r.Path,
+					}
+					dbi.Create(&record)
+				}
+			}
+			return nil
+		})
+		log.Printf("Database migration complete")
+		blt.Close()
+	} else {
+		log.Fatal("Could not find old boltdb goshort.db")
+	}
+
 }
